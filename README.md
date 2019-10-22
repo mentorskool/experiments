11909fb32802ac302b8ce9d87f32573c653151e8
amitrv007 <amitrv007@gmail.com>
2019-10-22 22:44:03 +0530

    added new use-case for fetching users list from slack channel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # experiments
 random creative experiments of Mentorskool
+
+* **use-case-01.py : ** Code to fetch list of users belonging to a given channel on Slack
---
+++ use-case-01.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# ### Fetching list of members from a slack channel
+
+# ### References : 
+# * [Here](https://hackernoon.com/how-to-create-a-slack-bot-that-messages-all-members-of-a-workspace-in-8-minutes-32a5b52838be)
+
+from slackclient import SlackClient
+import pandas as pd
+
+SLACK_BOT_TOKEN = 'xoxb-483237336743-488112887538-W6SSWCFdGbtqc9xhgt4ksBUN'
+slack_client = SlackClient(SLACK_BOT_TOKEN)
+
+users = slack_client.api_call("users.list",channel="#class-discussions")
+df = pd.DataFrame(columns=['id','name','email','phone'])
+
+row = []
+i = 0
+for u in users['members']:
+    row.append(u['id'])
+    row.append(u['profile']['display_name'])
+    if 'email' in u['profile']:
+        row.append(u['profile']['email'])
+    else:
+        row.append('n/a')
+    if 'phone' in u['profile']:
+        row.append(u['profile']['phone'])
+    else:
+        row.append('n/a')
+    df.loc[i] = row
+    i+=1
+    print(row)
+    row = []
+
+df.to_csv('users_list.csv',index=False)
